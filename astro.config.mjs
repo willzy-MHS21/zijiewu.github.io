70dee70f046e5fb9b3f086142694086da159567c
Wilson Wu <zijiewu2001@gmail.com>
2026-01-05 01:12:41 -0500

    remove the base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://willzy-mhs21.github.io',
-  base: '/zijiewu.github.io/',
   vite: {
     plugins: [tailwindcss()]
   },
